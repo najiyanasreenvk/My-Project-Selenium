97cb688b33b83c9f5fc3befd080ea251b261122e
ADMIN <ADMIN@DESKTOP-6EN26E1.lan>
2025-07-11 18:43:41 +0530

    initial commit after rename

--- com.MySeleniumProject/src/main/java/Constant/ConstantClass.java
+++
@@ -1,8 +0,0 @@
-package Constant;
-
-public class ConstantClass {
-	public static final int implicitwaittimeout=10;
-	public static final int explicitwaittimeout=5;
-	public static final String propertiesfilepath="\\src\\main\\resources\\config.Properties";
-
-}
--- com.MySeleniumProject/src/test/java/Retry/RetryListener.java
+++
@@ -1,5 +0,0 @@
-package Retry;
-
-public class RetryListener {
-
-}
